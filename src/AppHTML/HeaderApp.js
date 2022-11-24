e60924a80003eacf1b5987f42914d763a75be785
Steven <steven@StevendeMacBook-Pro.local>
2022-11-24 22:38:56 +0800

    feat/package

--- src/AppHTML/HeaderApp.js
+++ src/AppHTML/HeaderApp.js
@@ -4,14 +4,6 @@ function HeaderApp() {
 
   return (
     <div className="headerApp">
-      {/* <a href="http://localhost:3000" className="headerLink">
-        <img  src="logo.png" alt="logo" className="headerimg" />
-      </a> */}
-      
-      {/* <a href="http://localhost:3000" className="headerLink">
-
-        <h1 className="headerH1">樂享大師國際音樂營</h1>
-      </a> */}
 
       <Link to="/" className="headerLink">
       <img  src="logo.PNG" alt="logo" className="headerimg" />
